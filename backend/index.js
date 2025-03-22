cd2c891dab03a0c91a04ba3674ea1e58dc371125
Alok Shukla <alokshukla6060@gmail.com>
2025-03-22 10:33:01 +0530

    package

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(cors({
     origin : process.env.FRONTEND_URL,
     credentials : true
-}))
+}));
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api',router)
